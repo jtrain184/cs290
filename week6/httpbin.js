15b9c086ed0069f74d40109fa4bcccce5c1cbe6e
Bman <Bman>
2017-05-14 18:31:11 -0400

    AJAX post form to httpbin

---
+++ week6/httpbin.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+  document.getElementById('colorSubmit').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+    var payload = {color:null};
+    payload.color = document.getElementById('colorInput').value;
+    console.log(payload);
+    req.open('POST', 'http://httpbin.org/post', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+        document.getElementById("postResponse").textContent = response.data;
+      } 
+      else {
+            console.log("Error in network request: " + req.statusText);
+      }
+    });
+    req.send(JSON.stringify(payload));
+    event.preventDefault();
+  });
+}
