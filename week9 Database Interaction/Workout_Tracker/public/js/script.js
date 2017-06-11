9cd6a60bb8577ed05cded4fae53c8dac56921ced
Bman <Bman>
2017-06-10 23:45:06 -0400

    Adding table id number to cells

--- week9 Database Interaction/Workout_Tracker/public/js/script.js
+++ week9 Database Interaction/Workout_Tracker/public/js/script.js
@@ -57,7 +57,7 @@ function buildTable(data){
 	console.log("buildTable	called");
     
     var fields = Object.keys(data[0]);
-    var tableID = data.id;
+    var tableID = data["id"];
     var workoutTable = document.getElementById("workouts");
 
     data.forEach(function(object){
