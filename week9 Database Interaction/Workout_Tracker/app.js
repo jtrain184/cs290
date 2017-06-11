27f0c619f3acac0bc9eb32e65287562a1b47db60
Bman <Bman>
2017-06-10 22:45:15 -0400

    Update function

--- week9 Database Interaction/Workout_Tracker/app.js
+++ week9 Database Interaction/Workout_Tracker/app.js
@@ -41,6 +41,29 @@ app.get('/insert',function(req,res,next){
   });
 });
 
+app.get('/update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+        [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        context.results = "Updated " + result.changedRows + " rows.";
+        res.render('home',context);
+      });
+    }
+  });
+});
+
 
 /*****    Initialize new table    *****/
 app.get('/reset-table',function(req,res,next){
